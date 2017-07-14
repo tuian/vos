c7a2ad35103d6d80ec1a338ba90425ba19df4078
21ki <21kixc@gmail.com>
2017-07-14 08:55:48 +0800

    vos3.2_install.sh

--- vos3.2_install.sh
+++ vos3.2_install.sh
@@ -417,3 +417,33 @@ http {
     #}
 
 }
+
+
+echo "Record the following License information:"
+
+echo "RPM Time:"
+
+/bin/rpm --qf %{INSTALLTIME} -q rpm
+
+echo ""
+
+echo "VOS Time:"
+
+/bin/rpm --qf %{INSTALLTIME} -q vos3000
+
+echo ""
+
+echo "JDK Time:"
+
+/bin/rpm --qf %{INSTALLTIME} -q jdk
+
+echo ""
+
+
+#!/bin/bash
+
+sed -i '/$RET = 0/a\\t\tsleep 5\n\t\tif [ -f $PIDFILE ]; then\n\t\t\tPID=`cat $PIDFILE`\n\t\t\tif [ -d /proc/$PID ]; then\n\t\t\t\tkill -HUP $PID\n\t\t\tfi\n\t\tfi' /etc/init.d/mbx3000d
+
+sed -i '/"$?" = 0/a\\t\tsleep 5\n\t\tif [ -f $CMPIDFILE ]; then\n\t\t\tPID=`cat $CMPIDFILE`\n\t\t\tif [ -d /proc/$PID ]; then\n\t\t\t\tkill -HUP $PID\n\t\t\tfi\n\t\tfi' /etc/init.d/ivrd
+
+reboot
