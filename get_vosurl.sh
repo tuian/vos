5eb433e06347849715a4aa83422a1ae1e27691cd
21ki <21kixc@gmail.com>
2017-07-24 14:10:31 +0800

    vosurl

--- get_vosurl.sh
+++ get_vosurl.sh
@@ -10,7 +10,6 @@ https://raw.githubusercontent.com/21ki/vosclient/master/VOS3000-client-v2.1.4.0.
 https://raw.githubusercontent.com/21ki/vosclient/master/VOS3000-client-v2.1.4.0_Chs_nobackdoor.exe
 https://raw.githubusercontent.com/21ki/vosclient/master/VOS3000-client-v2.1.6.00.exe
 
-
 ##阿里云oss站
 #安装包
 http://21k.oss-cn-qingdao.aliyuncs.com/vospag/vos2009-2.1.2.4.tar.gz
@@ -21,9 +20,15 @@ http://21k.oss-cn-qingdao.aliyuncs.com/vospag/vos3000_2.1.2.0.tar.gz
 http://21k.oss-cn-qingdao.aliyuncs.com/vospag/vos3000_2.1.2.4.tar.gz
 http://21k.oss-cn-qingdao.aliyuncs.com/vospag/vos3000_2.1.3.2.tar.gz
 
-
-#内网站
-
+#阿里云oss内网站
+http://21k.oss-cn-qingdao-internal.aliyuncs.com/vospag/vos2009-2.1.2.4.tar.gz
+http://21k.oss-cn-qingdao-internal.aliyuncs.com/vospag/vos2009-2.1.2.4.tar.gz
+http://21k.oss-cn-qingdao-internal.aliyuncs.com/vospag/vos2009_32.tar.gz
+http://21k.oss-cn-qingdao-internal.aliyuncs.com/vospag/vos2009_64.tar.gz
+http://21k.oss-cn-qingdao-internal.aliyuncs.com/vospag/vos3000-2.1.4.0.tar.gz
+http://21k.oss-cn-qingdao-internal.aliyuncs.com/vospag/vos3000_2.1.2.0.tar.gz
+http://21k.oss-cn-qingdao-internal.aliyuncs.com/vospag/vos3000_2.1.2.4.tar.gz
+http://21k.oss-cn-qingdao-internal.aliyuncs.com/vospag/vos3000_2.1.3.2.tar.gz
 
 #vosclient
 http://21k.oss-cn-qingdao.aliyuncs.com/vosclient/VOS2009-client-v2.1.2.0.exe
