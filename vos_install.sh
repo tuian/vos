df74e8a495cd5b483b2f36714a18958900dd1263
21ki <21kixc@gmail.com>
2017-07-28 15:12:45 +0800

    vos2.0

--- vos_install.sh
+++ vos_install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 echo "Starting install VOS2009 System"
-IP=vos.ucserver.org
-FILE=vos2009.2.1.2.0.tar.gz
+IP=21k.oss-cn-qingdao.aliyuncs.com/vospag
+FILE=vos2009_32.tar.gz
 VOS2009_FILE=$FILE
 URL=http://$IP/$VOS2009_FILE
 echo $URL
@@ -9,10 +9,11 @@ OS=`head -n 1 /etc/issue |awk '{ print $3}'`
 echo $OS
 check_version()
 {
-if [ "$OS" != 5.5 ]
+if [ "$OS" -lt 6.0 ]
 then
-        echo "CentOS version must be 5.5"
+        echo "CentOS version must be 5.x"
         exit 0
+
 fi
 }
 download_vos()
@@ -22,18 +23,36 @@ download_vos()
 }
 install_rpm()
 {
-        rpm -ivh jdk.i586.rpm
+        rpm -ivh perl-DBI-1.40-5.i386.rpm --nodeps --force
         rpm -ivh MySQL-server-community-5.0.51a-0.rhel5.i386.rpm --nodeps --force
         rpm -ivh MySQL-client-community-5.0.51a-0.rhel5.i386.rpm --nodeps --force
-        rpm -ivh perl-DBI-1.40-5.i386.rpm --nodeps --force
-        mysql -uroot -e 'create database vosdb DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;'
+        rpm -ivh  jdk-1_5_0_08-linux-i586.rpm --nodeps --force
+        tee /etc/my.cnf <<-'EOF'
+	[client]
+	port=3306
+	default-character-set=utf8
+	[mysqld]
+	default-character-set=utf8
+	max_connections=160
+	interactive_timeout=310000
+	wait_timeout=31000
+	query_cache_size=48M
+	table_cache=320
+	tmp_table_size=52M
+	thread_cache_size=8
+	sort_buffer_size=256K
+	innodb_thread_concurrency=8
+	myisam-recover=FORCE
+	max_allowed_packet=32M
+	innodb_file_per_table=1
+	EOF
         chkconfig --level mysqld 2345 on
 }
 tar_tomcat()
 {
         PWD=`pwd`
-        mv $PWD/apache-tomcat-5.5.15.tar.bz2  /usr
-        tar -xvjf /usr/apache-tomcat-5.5.15.tar.bz2 -C /usr
+        #mv $PWD/apache-tomcat-5.5.15.tar.bz2  /usr
+        tar zxvf apache-tomcat-5.5.15.tar.gz -C /usr/
 }
 java_home()
 {
@@ -73,10 +92,10 @@ check_version
 download_vos
 install_rpm
 tar_tomcat
-java_home
+#java_home
 source /etc/profile
 install_vos
 make_license
-echo " Congratulation! VOS2009 has been installed successfully!"
+echo "Congratulation! VOS2009 has been installed successfully!"
 echo "Contact me if you want to crack the vos2009 2.1.2.0"
 echo "My QQ: 85959493"
