f4b89693eda70f7beed596b8c3680d4df1f98622
21ki <21kixc@gmail.com>
2017-11-15 13:14:33 +0800

    微信报警昆石攻击

--- 微信报警昆石攻击.sh
+++ 微信报警昆石攻击.sh
@@ -10,6 +10,9 @@ tee  /opt/restartvos.sh <<-'EOF'
 #!/bin/bash
 CC=`top -b1 -n1 | sed '1,5d' | awk '{if($9>=80.00)print}' | awk '{print $1}' |wc -l`
 MBX=`netstat -anlp|grep mbx|wc -l`
+AST=`ps -ef|grep asterisk|grep -v grep|wc -l`
+FS=`ps -ef|grep freeswitch|grep -v grep|wc -l`
+IP=`ifconfig  | grep -v '127.0.0.1' | awk '/inet addr/ {split($2,x,":");print x[2]}'|egrep -v "^10|^172"`
 #列出cpu大于80%的数量(可以根据自己服务器负载设置数值)
 if [ "$CC" -gt "0" ] || [ "$MBX" -eq "0" ];then
 #vos如果超过80%要重启服务,这里以vos2009 2.0为例具体依照自己安装版本为例
@@ -17,7 +20,9 @@ if [ "$CC" -gt "0" ] || [ "$MBX" -eq "0" ];then
 /etc/init.d/mbx2009d restart >/dev/null
 /etc/init.d/ivrd restart >/dev/null
 bash /opt/wexin.sh test test 前方高能预警\\n昆石正在攻击
-else 
+elif [ "$FS" -gt "0" ] || [ "$AST" -gt "0" ];then
+bash /opt/wexin.sh test test "$IP\\n可能正在被盗打\\n发出高能警告"
+else
 echo `date "+%Y-%m-%d %H:%M:%S"`  "VOS正常" >> /var/log/kill.log
 fi
 EOF
